f0b62d814dcc0abcde7c94756834c765ce22b0ce
bong <jungcome7@gmail.com>
2024-10-19 21:50:46 +0900

    feat: 채팅 생성 버튼 클릭시 chat id, input value 초기화

--- app/components/ChatList/CreateChatButton.tsx
+++ app/components/ChatList/CreateChatButton.tsx
@@ -1,9 +1,23 @@
+import { inputValueAtom, selectedChatIdAtom } from '@/app/atoms/chat';
 import { css } from '@/styled-system/css';
 import { Button } from '@radix-ui/themes';
+import { useSetAtom } from 'jotai';
 
 function CreateChatButton() {
+  const setSelectedChatId = useSetAtom(selectedChatIdAtom);
+  const setInputValue = useSetAtom(inputValueAtom);
+
+  const handleClick = () => {
+    setSelectedChatId(null);
+    setInputValue('');
+  };
+
   return (
-    <Button variant="outline" className={css({ alignSelf: 'end', cursor: 'pointer' })}>
+    <Button
+      variant="outline"
+      className={css({ alignSelf: 'end', cursor: 'pointer' })}
+      onClick={handleClick}
+    >
       New
     </Button>
   );
